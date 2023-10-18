8f8270d376fd81bb78bb452079cc88e542586bf5
Malcome Baggett <malcomebaggett@gmail.com>
2023-10-18 09:41:15 -0400

    fixed solution 197

--- joins/Leetcode197.sql
+++ joins/Leetcode197.sql
@@ -18,3 +18,7 @@
 -- Return the result table in any order.
 
 -- The result format is in the following example.
+SELECT e.name, b.bonus
+FROM Employee e
+LEFT JOIN Bonus b ON b.empId = e.empId
+WHERE b.bonus < 1000  OR b.bonus IS NULL
